f34fc1327150659750b2c950a9e504252a8d5ab7
Hyafinthus <hoyifu@gmail.com>
2020-05-05 15:51:03 +0800

    Hyf - Node节点属性 - Syntax完好 - Semantic差GUI

--- src/semantic/Node.java
+++ src/semantic/Node.java
@@ -1,7 +1,9 @@
 package semantic;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Node {
   public String data; // 非终结符 终结符token 语义动作字符串
@@ -18,6 +20,9 @@ public class Node {
   // ========== ========== ========== ========== ========== ========== ========== ==========
   // 是语义动作
   public boolean action = false;
+
+  // 节点属性
+  public Map<String, String> attr = new HashMap<>();
 
   public Node(String data) {
     this.data = data;
